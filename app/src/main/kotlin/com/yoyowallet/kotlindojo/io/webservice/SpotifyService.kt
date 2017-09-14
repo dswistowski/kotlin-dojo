e52df017f34b31244b084965fa56bb0fd056a53e
Tariq Patel <tariq@yoyowallet.com>
2017-09-14 12:25:18 +0100

    Initial BROWSE apis for spotify service

--- app/src/main/kotlin/com/yoyowallet/kotlindojo/io/webservice/SpotifyService.kt
+++ app/src/main/kotlin/com/yoyowallet/kotlindojo/io/webservice/SpotifyService.kt
@@ -1,4 +1,64 @@
 package com.yoyowallet.kotlindojo.io.webservice
 
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.QueryMap
+
 interface SpotifyService {
+
+    /**
+     * Get a list of Spotify featured playlists (shown, for example, on a Spotify player’s “Browse” tab).
+     *
+     * @param options  Optional parameters. For list of supported parameters see
+     *                 <a href="https://developer.spotify.com/web-api/get-list-featured-playlists/">endpoint documentation</a>
+     * @return A callback containing object with the featured playlists
+     * @see <a href="https://developer.spotify.com/web-api/get-list-featured-playlists/">Get a List of Featured Playlists</a>
+     */
+    @GET("/browse/featured-playlists")
+    fun getFeaturedPlaylists(@QueryMap options: Map<String, Any>?): Call<Unit>
+
+    /**
+     * Get a list of new album releases featured in Spotify (shown, for example, on a Spotify player’s “Browse” tab).
+     *
+     * @param options Optional parameters. For list of supported parameters see
+     *                <a href="https://developer.spotify.com/web-api/get-list-new-releases/">endpoint documentation</a>
+     * @return A callback containing object with the new album releases
+     * @see <a href="https://developer.spotify.com/web-api/get-list-new-releases/">Get a List of New Releases</a>
+     */
+    @GET("/browse/new-releases")
+    fun getNewReleases(@QueryMap options: Map<String, Any>?): Call<Unit>
+
+    /**
+     * Retrieve Spotify categories. Categories used to tag items in
+     * Spotify (on, for example, the Spotify player’s “Browse” tab).
+     *
+     * @param options Optional parameters.
+     * @return A callback containing categories.
+     * @see <a href="https://developer.spotify.com/web-api/get-list-categories/">Get a List of Categories</a>
+     */
+    @GET("/browse/categories")
+    fun getCategories(@QueryMap options: Map<String, Any>?): Call<Unit>
+
+    /**
+     * Retrieve a Spotify category.
+     *
+     * @param categoryId The category's ID.
+     * @param options    Optional parameters.
+     * @return A callback containing a Spotify category.
+     * @see <a href="https://developer.spotify.com/web-api/get-category/">Get a Spotify Category</a>
+     */
+    @GET("/browse/categories/{category_id}")
+    fun getCategory(@Path("category_id") categoryId: String, @QueryMap options: Map<String, Any>?): Call<Unit>
+
+    /**
+     * Retrieve playlists for a Spotify Category.
+     *
+     * @param categoryId The category's ID.
+     * @param options    Optional parameters.
+     * @return A callback containing playlists for a Spotify Category.
+     * @see <a href="https://developer.spotify.com/web-api/get-categorys-playlists/">Get playlists for a Spotify Category</a>
+     */
+    @GET("/browse/categories/{category_id}/playlists")
+    fun getPlaylistsCategory(@Path("category_id") categoryId: String, @QueryMap options: Map<String, Any>?): Call<Unit>
 }
